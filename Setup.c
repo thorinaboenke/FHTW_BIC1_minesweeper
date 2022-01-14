fccdeeb0b16d319f8523ae5a84ac27426559aa91
Thorina Boenke <thorina-boenke@web.de>
2022-01-14 14:46:18 +0100

    documentation for Setup.c

--- Setup.c
+++ Setup.c
@@ -1,3 +1,5 @@
+/** \file
+* \brief This file handles setting up a new playfield for minesweeper*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,27 +10,40 @@ struct cell** field;
 int grid_size;
 int no_of_mines;
 
+/**
+ Generates new playing field.
+
+ Allocates memory dynamically for a nested array of Cells according to grid size.
+ Initializes all flags of the cell structs with 0.
+ Places mines randomly on the field with placeMine().
+ Determines the number of adjacent mines for each cell.
+ Randomly opens one cell that is not a mine with openFirstCell().
+
+ @returns pointer to a nested array of cells ( i.e. the newly generated field)
+
+ */
+
 struct cell** generateField(){
-//dynamically allocate memory for a nested array of fields according to grid size
+// dynamically allocate memory for a nested array of Cell according to grid size
 
   int g = grid_size, i, j;
 
   struct cell** field = (struct cell**)malloc(g * sizeof(struct cell*));
   if (field == NULL){
-    fprintf(stderr, "Out of memory\n");
+    fprintf(stderr, "Could not allocate memory\n");
     exit(1);
   }
   for (i = 0; i < g; i++){
     field[i] = (struct cell*)malloc(g * sizeof(struct cell));
     if (field[i] == NULL){
-    fprintf(stderr, "Out of memory\n");
+    fprintf(stderr, "Could not allocate memory\n");
     exit(1);
   }
   }
-  //initialize with default values
+  // initialize with default values
   for (i = 0; i < g; i++){
     for (j = 0; j < g; j++){
-      field[i][j] = (struct cell) {.mine= 0, .adjacent_mines = 0, .isOpened = 0, .isFlagged = 0};
+      field[i][j] = (struct cell) {.mine = 0, .adjacent_mines = 0, .isOpened = 0, .isFlagged = 0};
     }
   }
 
@@ -37,7 +52,7 @@ struct cell** generateField(){
     placeMine(field);
   }
 
-  // save number of adjacent mines
+  // determine and save number of adjacent mines
   // always check before that index is not out of bounds
   for (int i = 0 ; i < grid_size; i++ ){
     for (int j = 0 ; j < grid_size; j++ ){
@@ -87,11 +102,20 @@ struct cell** generateField(){
     }
   }
 
-  //randomly open one field that is not a mine
+  // randomly open one cell that is not a mine
   openFirstCell(field);
   return field;
 };
 
+
+/**
+ Places a mine on the field.
+
+ Generates random x and y coordinates.
+ Checks that there is no mine on that cell yet and places a mine, else calls itself recursively until a free cell is targeted.
+ (max. half the number of cells will contain a mine, so the recursion will eventually end)
+
+ */
 void placeMine(struct cell** field){
   int x = rand() % grid_size;
   int y = rand() % grid_size;
@@ -102,6 +126,14 @@ void placeMine(struct cell** field){
   }
 }
 
+/**
+ Opens the first cell.
+
+ Generates random x and y coordinates.
+ Checks that there is no mine on that cell and opens it, else calls itself recursively until a cell is targeted that does not contain a mine.
+ (max. half the number of cells will contain a mine, so the recursion will eventually end)
+
+ */
 void openFirstCell(struct cell** field){
   int x = rand() % grid_size;
   int y = rand() % grid_size;
